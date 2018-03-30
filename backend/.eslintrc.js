67ddf882768e42ede08d75ade200553499a15478
anna <anya.pogorelova@gmail.com>
2018-03-30 11:59:52 +0300

    Added .eslintrc.js file.

---
+++ backend/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2017
+    },
+    "rules": {
+        "no-console": "off",
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};

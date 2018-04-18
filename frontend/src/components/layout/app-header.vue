216046b4dcd0bab77d3473587220a5374d1abf5f
anna <anya.pogorelova@gmail.com>
2018-04-18 12:05:37 +0300

    Removed unused links from the navbar.

--- frontend/src/components/layout/app-header.vue
+++ frontend/src/components/layout/app-header.vue
@@ -4,15 +4,6 @@
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <router-link class="nav-link" to="/sign-in">Sign In</router-link>
-                </li>
-                <li class="nav-item">
-                    <router-link class="nav-link" to="/register">Register</router-link>
-                </li>
-            </ul>
-        </div>
+        <div class="collapse navbar-collapse" id="navbarNav"></div>
     </nav>
 </template>

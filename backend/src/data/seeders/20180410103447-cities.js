365e27070f381d6a4947f1c9620bdb9f4e4e6ef1
anna <anya.pogorelova@gmail.com>
2018-04-18 14:04:34 +0300

    Fixed seeder, added new cities.

--- backend/src/data/seeders/20180410103447-cities.js
+++ backend/src/data/seeders/20180410103447-cities.js
@@ -1,20 +1,37 @@
 "use strict";
 
+const db = require("../models/index");
+
 module.exports = {
-    up: (queryInterface, Sequelize) => {
-        return queryInterface.bulkInsert("city", [{
+    up: async () => {
+        const cities = [{
             name: "Львів",
             nameEn: "Lviv",
-            coordinates: Sequelize.fn("ST_GeomFromText", "POINT(49.8327786 23.9420238)", "4326"),
+            coordinates: [49.8327786, 23.9420238]
         }, {
             name: "Володимир-Волинський",
             nameEn: "Volodymyr-Volynskyi",
-            coordinates: Sequelize.fn("ST_GeomFromText", "POINT(50.8482214 24.2869926)", "4326")
+            coordinates: [50.8482214, 24.2869926]
         }, {
             name: "Жовква",
             nameEn: "Zhovkva",
-            coordinates: Sequelize.fn("ST_GeomFromText", "POINT(50.0549683 23.94149476)", "4326")
-        }], {});
+            coordinates: [50.0549683, 23.94149476]
+        }, {
+            name: "Луцьк",
+            nameEn: "Lutsk",
+            coordinates: [50.7465708, 25.2818164]
+        }, {
+            name: "Івано-Франківськ",
+            nameEn: "Ivano-Frankivsk",
+            coordinates: [48.9118646, 24.6469172]
+        }];
+
+        for(let city of cities) {
+            let existingCity = await db.city.findOne({where: {name: city.name}});
+            if(!existingCity) {
+                await db.city.create(city);
+            }
+        }
     },
 
     down: (queryInterface) => {

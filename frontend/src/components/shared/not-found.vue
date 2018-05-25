08d1cd1da4f173ffe27b570ca68daf17f64efadb
anna <anya.pogorelova@gmail.com>
2018-05-25 16:29:25 +0300

    Fixed CSS scope.

--- frontend/src/components/shared/not-found.vue
+++ frontend/src/components/shared/not-found.vue
@@ -1,10 +1,18 @@
 <template>
     <div class="row page-wrapper">
         <div class="col-12">
-            <h3>Сторіночка не знайдена</h3>
+            <h3>Вибачте, але ця сторіночка не знайдена</h3>
+            <router-link to="/" class="btn btn-outline-success" title="На головну" aria-label="На головну">
+                На головну
+            </router-link>
         </div>
     </div>
 </template>
+<style scoped>
+    .btn {
+        margin-top: 10px;
+    }
+</style>
 <script>
     export default {
 

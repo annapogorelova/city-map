8bbf4c567ad94293fa37afd4ec98a27d8e32103d
anna <anya.pogorelova@gmail.com>
2018-06-16 13:38:45 +0300

    Changed logging driver to Console.

--- backend/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ! -f .initialized ]; then
-    echo "Initializing container"
-    mkdir -p -m 755 /home/node/city-map/logs/
-    touch .initialized
-fi
-
-exec "$@"
--- backend/src/app.js
+++ backend/src/app.js
@@ -8,39 +8,17 @@ const cookieParser = require("cookie-parser");
 const helmet = require("helmet");
 const config = require("config");
 const errorHandler = require("./http/middleware/errorHandler");
-const fs = require("fs");
 const expressWinston = require("express-winston");
-const DailyRotateFile = require("winston-daily-rotate-file");
+const winston = require("winston");
 
 const routes = dc.get("Router");
 const app = express();
 
 // Request logger for production
 
-if (process.env.NODE_ENV === "production") {
-    const logDirectory = path.join(__dirname, config.get("logsPath"));
-    if(!fs.existsSync(logDirectory)) {
-        fs.mkdirSync(logDirectory);
-    }
-
-    const transport = new (DailyRotateFile)({
-        dirname: logDirectory,
-        filename: "web-%DATE%.log",
-        datePattern: "YYYY-MM-DD-HH",
-        zippedArchive: true,
-        maxSize: "20m",
-        maxFiles: "30d"
-    });
-
-    app.use(expressWinston.logger({
-        transports: [transport],
-        meta: true,
-        expressFormat: true,
-        colorize: false
-    }));
-} else {
-    app.use(logger("dev"));
-}
+app.use(expressWinston.logger({
+    transports: [new winston.transports.Console()]
+}));
 
 app.use(cookieParser());
 app.use(helmet());
@@ -62,22 +40,9 @@ app.use("/api/v1", routes);
 
 // Error logger for production
 
-if (process.env.NODE_ENV === "production") {
-    const logDirectory = path.join(__dirname, config.get("logsPath"));
-
-    const transport = new (DailyRotateFile)({
-        dirname: logDirectory,
-        filename: "errors-%DATE%.log",
-        datePattern: "YYYY-MM-DD-HH",
-        zippedArchive: true,
-        maxSize: "20m",
-        maxFiles: "30d"
-    });
-
-    app.use(expressWinston.errorLogger({
-        transports: [transport]
-    }));
-}
+app.use(expressWinston.logger({
+    transports: [new winston.transports.Console()]
+}));
 
 app.use(errorHandler.handleError);
 

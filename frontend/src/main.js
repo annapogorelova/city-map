4c1d9026cae57b6dca149978e0bb89ac32c44f10
anna <anya.pogorelova@gmail.com>
2018-06-22 14:15:44 +0300

    Removed vue-analytics, added google analytics code straight to the index.html.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,6 @@
 import "./polyfills";
 
 import Vue from "vue";
-import VueAnalytics from "vue-analytics";
 import App from "./app";
 import router from "./router/router";
 import "bootstrap/dist/css/bootstrap.css";
@@ -10,25 +9,10 @@ import "../assets/css/fontawesome-all.min.css";
 import "../assets/css/tables.css";
 import dc from "./dependency-container";
 import "./filters/index";
-import appConfig from "./app.config";
 
 Vue.config.productionTip = false;
 Vue.prototype.$dc = dc;
 
-if (appConfig.googleAnalyticsId) {
-    Vue.use(VueAnalytics, {
-        id: appConfig.googleAnalyticsId,
-        checkDuplicatedScript: true,
-        router,
-        autoTracking: {
-            transformQueryString: false,
-            skipSamePath: true
-        },
-        debug: {
-            sendHitTask: process.env.NODE_ENV === "production"
-        }
-    });
-}
 
 new Vue({
     el: "#app",
